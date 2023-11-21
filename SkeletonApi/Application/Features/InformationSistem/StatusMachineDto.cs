8d0995d37b03a9619622a4901bead2eeff28c005
MUH AGUS SALIM <aguh000000@gmail.com>
2023-11-21 15:52:12 +0700

    commit

---
+++ SkeletonApi/Application/Features/InformationSistem/StatusMachineDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SkeletonApi.Application.Features.InformationSistem
+{
+   public class StatusMachineDto
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; init; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; init; }
+
+        public int Number { get; init; }
+
+        [JsonPropertyName("value")]
+        public byte Value { get; init; }
+
+        [JsonPropertyName("date_time")]
+        public string DateTimeString { get; init; }
+
+        [JsonIgnore]
+        public DateTime? DateTime { get; init; }
+    }
+}
