1bbb69c248243483f7fdc6c38c96df520838bc8d
adeputri99 <140032326+adeputri99@users.noreply.github.com>
2023-12-22 10:10:22 +0700

    .
    .

--- SkeletonApi/Domain/Entities/Tsdb/EnginePart.cs
+++ SkeletonApi/Domain/Entities/Tsdb/EnginePart.cs
@@ -1,13 +1,7 @@
-﻿using SkeletonApi.Domain.Common.Abstracts;
-using SkeletonApi.Domain.Common.Abstracts.Tsdb;
-using System;
-using System.Collections.Generic;
+﻿using SkeletonApi.Domain.Common.Abstracts.Tsdb;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace SkeletonApi.Domain.Entities.Tsdb
 {
